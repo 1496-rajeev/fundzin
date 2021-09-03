0f0f11b1f5952d450b74f876b6bcfc42deaf4ef1
1496-rajeev <1496piku@gmail.com>
2021-09-03 17:12:34 +0530

    updated

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,7 +1,18 @@
 console.log("custom js");
 
-const nextBtn = document.getElementById("nextBtn");
-
-nextBtn.addEventListener("click", function () {
+function onformSubmit() {
+  submitted = true;
   localStorage.setItem("form_submitted", submitted);
-});
+}
+
+function showHidealert() {
+  console.log("func call on pageload");
+  console.log(localStorage.getItem("form_submitted"));
+  if (localStorage.getItem("form_submitted") === "true") {
+    var element = document.getElementById("success_alert");
+    element.classList.add("show");
+  } else {
+    var element = document.getElementById("success_alert");
+    element.classList.add("hide");
+  }
+}
